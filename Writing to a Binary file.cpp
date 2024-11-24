fddb2e94983dde86e05dd18fc4ee92fa14c1a538
jagrutispawar <jagrutipawar0604@gmail.com>
2024-11-24 21:24:19 +0530

    Add files via upload
    This C++ program writes the binary data of a Data structure to a file named data.bin. It uses ofstream in binary mode to save the structure's contents and displays a success or error message based on the file's status.

---
+++ Writing to a Binary file.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Data {
+int id;
+char name[50];
+};
+int main() {
+Data d1 = {1, "John Doe"};
+ofstream outFile("data.bin", ios::binary); 
+
+if (outFile.is_open()) {
+outFile.write(reinterpret_cast<char*>(&d1), sizeof(d1));
+
+outFile.close();
+cout << "Data written to binary file." << endl;
+} else {
+cout << "Error: Could not open file." << endl;
+}
+return 0;
+}
